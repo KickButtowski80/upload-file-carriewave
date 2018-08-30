6f2e3b5481fe1239ba245f1f1dd94d3d5bd9f4ae
kickbuttowski59 <pazpaz22@yahoo.com>
2018-08-30 23:21:05 +0000

    deployed file

---
+++ test/controllers/resumes_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ResumesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get resumes_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get resumes_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get resumes_create_url
+    assert_response :success
+  end
+
+  test "should get deostry" do
+    get resumes_deostry_url
+    assert_response :success
+  end
+
+end
